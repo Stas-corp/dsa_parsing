c3143ea61bac9594ed4b91879f5d3339a2503b37
Stas-corp <zakyska1301@gmail.com>
2025-08-19 14:28:42 +0300

    scrap & extract archivs

---
+++ scrap.py
@@ -0,0 +1,73 @@
+import requests
+import zipfile
+import shutil
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://dsa.court.gov.ua"
+START_URL = f"{BASE_URL}/dsa/inshe/oddata/532/?page=1"
+
+
+def get_archives():
+    page = 3
+    urls_zip = []
+    date_zip = []
+    while True:
+        url = f"{BASE_URL}/dsa/inshe/oddata/532/?page={page}"
+        resp = requests.get(url)
+        resp.raise_for_status()
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        items = soup.find_all("div", class_="allFiles")
+        # print(f"items -> {items}")
+        if not items:
+            break
+
+        stop = False
+        splash = False
+        for link in items:
+            item = link.find("a")
+            text = item.get("download")
+            href = item.get("href")
+            # print(f"{text}\n{href}")
+            if not href or not href.endswith(".zip"):
+                continue
+            if "2025" in text:
+                urls_zip.append(href)
+                date_zip.append(text.split('від')[-1])
+                if splash:
+                    stop = True
+            elif "2024" in text:
+                splash = True
+                continue
+        # print(result)
+        if stop:
+            break
+        page += 1
+    return list(zip(urls_zip, date_zip))
+
+
+def process_archive(url, date):
+    print(f"Downloading: {url}")
+    tmpdir = Path('zip_dir')
+    if not tmpdir.exists():
+        tmpdir.mkdir()
+    local_zip = tmpdir / date
+
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_zip, "wb") as f:
+            shutil.copyfileobj(r.raw, f)
+
+    with zipfile.ZipFile(local_zip, "r") as zf:
+        zf.extractall(tmpdir / str(date[:-4]+'_unpack'))
+
+
+def main():
+    archives = get_archives()
+    print(f"Found {len(archives)} archives")
+    for url, date in archives:
+        process_archive(url, date)
+
+if __name__ == "__main__":
+    main()
