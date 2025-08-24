a50ef0c2e4a165e8f5b2b41550386fffca9fcc6a
Stas-corp <zakyska1301@gmail.com>
2025-08-24 12:38:19 +0300

    BONUS: add data export from csv cases

---
+++ get_cases_from_csv.py
@@ -0,0 +1,4 @@
+from db.csv_proccesing import test_main, get_cases
+
+if __name__ == "__main__":
+    get_cases('сases.csv')
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from app.scrap import main
-# from db.csv_proccesing import main
 
 if __name__ == "__main__":
     main()
